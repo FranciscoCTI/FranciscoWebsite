46ca45882b187d2719167f97b9b2091089911fe1
FranciscoCTI <franciscofcch@gmail.com>
2026-01-19 08:49:32 -0300

    Working on editing technology

--- backend/models/technology.model.js
+++ backend/models/technology.model.js
@@ -11,7 +11,7 @@ const technologySchema = new mongoose.Schema(
             required: true,
         },
         year: {
-            type: String,
+            type: Number,
             required: true,
         },
         userLevel: {
